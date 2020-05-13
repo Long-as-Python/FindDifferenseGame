ad900e34cad609b1f85e3bb4bdc453fb38fc46e3
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-05-13 21:39:50 +0300

    image spawner and button spawner working
    also imported text mesh pro

---
+++ FindDifferenseGame/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    Image img;
+    public Levels[] levels;
+    public Transform parent;
+
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        img.sprite = levels[0].sprite;
+        Instantiate(levels[0].buttons.button, Vector3.zero, Quaternion.identity).transform.SetParent(parent);
+
+    }
+
+    [System.Serializable]
+    public class Buttons
+    {
+        public float coordX;
+        public float coordY;
+        public Button button;
+    }
+
+    [System.Serializable]
+    public class Levels
+    {
+        public Sprite sprite;
+        public Buttons buttons;
+    }
+}
