1b614b08f1afee2b7f1d377150efc1181969c74d
Long-as-Python <45416328+Long-as-Python@users.noreply.github.com>
2020-05-16 17:53:02 +0300

    final version
    project is ready

--- FindDifferenseGame/Assets/Scripts/LevelLoader.cs
+++ FindDifferenseGame/Assets/Scripts/LevelLoader.cs
@@ -5,25 +5,62 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-
     Image img;
+    public Transform canvas;
     public Levels[] levels;
     public Transform parent;
+    public int onGoingLevel = 0;
+    public int buttonCount = 0;
+    public GameObject Game;
+    public GameObject MainMenu;
 
 
-    void Start()
+    public void Start()
     {
         img = GetComponent<Image>();
-        img.sprite = levels[0].sprite;
-        Instantiate(levels[0].buttons.button, Vector3.zero, Quaternion.identity).transform.SetParent(parent);
+    }
 
+    public void Update()
+    {
+        if (buttonCount == 0 && onGoingLevel == levels.Length)
+        {
+            onGoingLevel = 0;
+            Game.SetActive(false);
+            MainMenu.SetActive(true);
+            return;
+        }
+        if (buttonCount == 0 && onGoingLevel < levels.Length)
+        {
+            Levelloader();
+            onGoingLevel++;
+        }
+    }
+
+    public void Levelloader()
+    {
+        img.sprite = levels[onGoingLevel].sprite;
+        ButtonPlacer(onGoingLevel);
+        buttonCount = levels[onGoingLevel].buttons.Length;
+    }
+
+    public void ButtonCounter()
+    {
+        buttonCount--;
+        Debug.Log("isWorking");
+    }
+
+    public void ButtonPlacer(int _onGoingLevel)
+    {
+        for (int i = 0; i < levels[_onGoingLevel].buttons.Length; i++)
+        {
+            Instantiate(levels[_onGoingLevel].buttons[i].button, canvas.position + levels[_onGoingLevel].buttons[i].buttonCoords, Quaternion.identity, canvas).onClick.AddListener(ButtonCounter);
+        }
     }
 
     [System.Serializable]
     public class Buttons
     {
-        public float coordX;
-        public float coordY;
+        public Vector3 buttonCoords;
         public Button button;
     }
 
@@ -31,6 +68,6 @@ public class LevelLoader : MonoBehaviour
     public class Levels
     {
         public Sprite sprite;
-        public Buttons buttons;
+        public Buttons[] buttons;
     }
 }
---
+++ FindDifferenseGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Game;
+    public GameObject MainMenu;
+
+
+    public void PlayGame()
+    {
+        Game.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+
+    }
+}
